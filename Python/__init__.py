b6c84f0194576170e2a8c0e800060e70dca3f1e8
Nicholas Corgan <n.corgan@gmail.com>
2019-12-19 16:51:23 -0600

    Revert directirng Python warnings to Poco::Logger, this moved to Pothos itself

--- Python/__init__.py
+++ Python/__init__.py
@@ -8,13 +8,3 @@ from .Random import *
 from .RegisteredCallHelpers import *
 from .Save import *
 from .Utility import *
-
-import Pothos
-import logging
-
-# logging.captureWarnings() redirects all outputs from the "warnings" module
-# to a Python logger named "py.warnings". Adding our log handler to this logger
-# results in all NumPy warnings being consumed by our infrastructure.
-logging.basicConfig(level=logging.INFO)
-logging.getLogger("py.warnings").addHandler(Pothos.LogHandler("py.warnings"))
-logging.captureWarnings(True)
