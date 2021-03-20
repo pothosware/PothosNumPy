c4b890e649be772b8519bd0b3c105377ee384e85
Nicholas Corgan <n.corgan@gmail.com>
2021-03-19 20:39:22 -0500

    For consistency with other modules, perform version check on block import instead of module import

---
+++ Python/__init__.py
@@ -0,0 +1,13 @@
+# Copyright (c) 2019-2021 Nicholas Corgan
+# SPDX-License-Identifier: BSD-3-Clause
+
+from .BlockEntryPoints import *
+from .FFT import *
+from .FileSink import *
+from .FileSource import *
+from .Random import *
+from .RegisteredCallHelpers import *
+from .Utility import *
+from .Window import *
+
+from . import TestFuncs
